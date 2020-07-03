1827caedc0786b7ad2c17aacfb873c8cf437bda0
steve B <stephenbrousseau89@gmail.com>
2020-07-03 15:27:00 -0600

    syntax progress

--- src/python/syntax_test.py
+++ src/python/syntax_test.py
@@ -22,5 +22,33 @@ def test_accessDictionary():
     assert(persist.accessDictionary("name")=='test')
 
 def test_sampleIfElse():
-    assert(persist.sampleIfElse(2)==True, 'its even')
-    # assert(persist.sampleIfElse(3)==False, 'its even')
+    assert(persist.sampleIfElse(2)== 'its even')
+    assert(persist.sampleIfElse(1)== 'its odd')
+    assert(persist.sampleIfElse('S')== "not a number")
+
+def test_arrayFront():
+    array=[1,2,3]
+    assert(persist.arrayFront(array,4)==[4,1,2,3])
+    assert(persist.arrayFront(array,5)==[5,4,1,2,3])
+
+def test_arrayBack():
+    array=[1,2,3]
+    assert(persist.arrayBack(array,4)==[1,2,3,4])
+    assert(persist.arrayBack(array,5)==[1,2,3,4,5])
+
+def test_arrayRemove():
+    array=[1,2,3]
+    assert(persist.arrayRemove(array,0)==[2,3])
+    assert(persist.arrayRemove(array,1)==[2])
+
+def test_updateArray():
+    array=[1,2,3]
+    assert(persist.updateArray(array,0,10)==[10,2,3])
+    assert(persist.updateArray(array,2,58)==[10,2,58])
+
+def test_forLoop():
+    assert persist.forLoop()=="steve","uranka"
+
+def test_whileLoop():
+    assert persist.whileLoop(5)=='thats a nice number, enter another'
+    # assert persist.whileLoop("Y",1)=="wrong"
