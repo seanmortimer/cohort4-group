a6178ed81201140e833b28176d98475e8ed774b2
Sean Mortimer <sean.mortimer@gmail.com>
2020-07-03 23:14:32 -0600

    Work on tax calculator

--- python/tax_calculator_test.py
+++ python/tax_calculator_test.py
@@ -1,6 +1,10 @@
 import tax_calculator as tax_calc
 
 def test_tax_calc():
+    # assert tax_calc.tax_calc(1) == 0.15
     assert tax_calc.tax_calc(100) == 15
-    assert tax_calc.tax_calc(1000) == 150
+    # assert tax_calc.tax_calc(1000) == 150
+    # assert tax_calc.tax_calc(48535) == 7280.25
+    assert tax_calc.tax_calc(48535.25) == 7280.30
     
+
