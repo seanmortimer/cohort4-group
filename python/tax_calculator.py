04788c6eb22caadec752a24ee03d1ba596ada8bc
Sean Mortimer <sean.mortimer@gmail.com>
2020-07-04 12:54:36 -0600

    Clean up tax calculator

--- python/tax_calculator.py
+++ python/tax_calculator.py
@@ -1,8 +1,5 @@
-from decimal import Decimal
-
 # brackets format: (rate, upper_limit)
 tax_brackets = [
-    # (0, 0),
     (15, 48535),
     (20.5, 97069),
     (26, 150473),
@@ -26,9 +23,12 @@ def tax_calc(gross):
             owed = round(owed_int) / 100
             return owed
 
-#  15% on the first $48,535 of taxable income, plus
-# 20.5% on the next $48,534 of taxable income (on the portion of taxable income over 48,535 up to $97,069), plus
-# 26% on the next $53,404 of taxable income (on the portion of taxable income over $97,069 up to $150,473), plus
-# 29% on the next $63,895 of taxable income (on the portion of taxable income over 150,473 up to $214,368), plus
+#  15% on the first $48,535 of taxable income
+# 20.5% on the next $48,534 of taxable income 
+#   (on the portion of taxable income over 48,535 up to $97,069), plus
+# 26% on the next $53,404 of taxable income 
+#   (on the portion of taxable income over $97,069 up to $150,473), plus
+# 29% on the next $63,895 of taxable income 
+#   (on the portion of taxable income over 150,473 up to $214,368), plus
 # 33% of taxable income over $214,368
 
