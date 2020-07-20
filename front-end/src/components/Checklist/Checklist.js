a02ff3e78cd8b0245bf0a62c1f548d260ecd0c22
Sean Mortimer <sean.mortimer@gmail.com>
2020-07-20 16:52:54 -0600

    Start adding symptoms to checklist

--- front-end/src/components/Checklist/Checklist.js
+++ front-end/src/components/Checklist/Checklist.js
@@ -15,6 +15,41 @@ import Container from '@material-ui/core/Container';
 
 // TODO: Add stepper component to show progress
 
+const checklist = {
+  page1: { list: [
+    'severe difficulty breathing (e.g., struggling for each breath, speaking in single words)',
+    'severe chest pain',
+    'having a very hard time waking up',
+    'feeling confused',
+    'lost consciousness',
+  ] },
+  page2: { list: [
+    'shortness of breath at rest',
+    'inability to lie down because of difficulty breathing',
+    'chronic health conditions that you are having difficulty managing because of your current respiratory illness',
+  ] },
+  page3: { list: [
+    'fever',
+    'new onset of cough or worsening of chronic cough',
+    'new or worsening shortness of breath',
+    'new or worsening difficulty breathing',
+    'sore throat',
+  ] },
+  page4: { list: [
+    'chills',
+    'painful swallowing',
+    'stuffy nose',
+    'headache',
+    'muscle or joint ache',
+    'feeling unwell, fatigue or severe exhaustion',
+    'nausea, vomiting, diarrhea or unexplained loss of appetite',
+    'loss of sense of smell or taste',
+  ] },
+  page5: { question: 'In the past 14 days, did you return from travel outside of Canada, or did you have'
+   + 'close contact with someone who is confirmed as having COVID-19?' },
+
+};
+
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
@@ -48,6 +83,7 @@ export default function Checklist() {
         <Typography component="h1" variant="h5">
           Covid-19 Screening Checklist
         </Typography>
+        <h1>Are you experiencing any of the following:</h1>
         <form className={classes.form} noValidate>
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
