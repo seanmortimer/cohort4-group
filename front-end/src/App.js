f971bf1c778fb80ace0ce69007c6d15b289402e8
Sean Mortimer <sean.mortimer@gmail.com>
2020-07-20 15:04:44 -0600

    Implement sidebard navigation

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import Register from './components/Register/Register';
+// import Register from './components/Register/Register';
+// import Checklist from './components/Checklist/Checklist';
+import Dashboard from './components/Dashboard/Dashboard';
 import './App.css';
 
 function App() {
+  // const [page, setPage] = useState(0);
   return (
-    <Register />
+    <Dashboard />
+    // <Checklist />
   );
 }
 
