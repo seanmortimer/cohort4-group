2660ca18cfb408d21c62aab2e4ecc41dd2360f4e
Sean Mortimer <sean.mortimer@gmail.com>
2020-07-22 10:22:47 -0600

    Test commit

--- front-end/src/components/Checklist/Checklist.js
+++ front-end/src/components/Checklist/Checklist.js
@@ -67,7 +67,7 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: '100%', // Fix IE 11 issue. 
     marginTop: theme.spacing(3),
   },
   submit: {
