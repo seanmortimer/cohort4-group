d5eebd77544bdb9c9809c6bc96b77403292becec
Uranka <39204253+urankab@users.noreply.github.com>
2020-08-31 18:27:52 -0600

    Working handleDelete in AdminDash

--- front-end/src/components/AdminDash/AdminDash.js
+++ front-end/src/components/AdminDash/AdminDash.js
@@ -3,35 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import { TextField, Typography, Button, Avatar, Grid } from '@material-ui/core';
 import AdminDisplay from '../AdminDisplay/AdminDisplay';
 import PageviewIcon from '@material-ui/icons/Pageview';
-
-// const useStyles = makeStyles((theme) => ({
-//   container: {
-//     display: 'grid',
-//     justifyContent: 'center',
-//     marginTop: 64
-//     // flexWrap: 'wrap',
-//   },
-//   avatar: {
-//     margin: theme.spacing(1),
-//     backgroundColor: theme.palette.secondary.main,
-//   },
-//   header: {
-//     textAlign: 'center'
-//   },
-//   form: {
-//     width: '100%', // Fix IE 11 issue.
-//     marginTop: theme.spacing(3),
-//     display: 'grid'
-//   },
-//   textField: {
-//     marginLeft: theme.spacing(1),
-//     marginRight: theme.spacing(1),
-//     width: 200,
-//   },
-//   submit: {
-//     margin: theme.spacing(3, 0, 2),
-//   },
-// }));
+import { deleteData } from '../../business/fetch'
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -80,6 +52,29 @@ export default function AdminDash() {
     data = await data.json();
     console.log(data)
     setQueryData(data.body)
+  }
+
+  const handleDelete = async (e) => {
+    let url = 'https://9ynldka4jk.execute-api.ca-central-1.amazonaws.com/dev/fetch-data'
+    let url2 = 'https://9ynldka4jk.execute-api.ca-central-1.amazonaws.com/dev/delete'
+    
+    var d = new Date();
+    let year = d.getFullYear()
+    let month = d.getMonth()
+    if (month<10){
+      month = `0${month}`
+    }
+  
+    let day = d.getDate()
+    let deletionStamp = `${year}-${month}-${day}`
+    let data = await fetch(`${url}?Start_Date='2020-03-05'&End_Date=${deletionStamp}`);
+    data = await data.json();
+    setQueryData(data.body)
+    console.log(queryData)
+
+    await queryData.map((item) => {
+      let stuff = deleteData(`${url2}?email=${item.email}&date=${item.date}`,{date:item.date, email:item.email});
+    })
   }
 
   var d = new Date();
@@ -148,10 +143,20 @@ export default function AdminDash() {
         >
           {dateStamp} Space Users
             </Button>
+        <Button
+          type="button"
+          variant="contained"
+          color="primary"
+          className={classes.submit}
+          onClick={handleDelete}
+        // disabled={!errorMsg.isValid}
+        >
+          Delete
+            </Button>
       </form>
       <ol>
         {queryData.map((item) => {
-          console.log(item)
+          // console.log(item)
           return <AdminDisplay item={item}
             key={item.key}
           // key={Object.keys(queryData)}
